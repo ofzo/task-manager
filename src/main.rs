3569cb4d0851ceab0137bcea1d9a74cede0a8761
roadup <fangwei.roadup@bytedance.com>
2021-04-30 12:02:43 +0800

    feat: use anyhow handle error

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
-mod cli;
+use anyhow::anyhow;
+use cli::{Action::*, CommandLineArgs};
+use std::path::PathBuf;
 use structopt::StructOpt;
+
+mod cli;
 mod tasks;
 
-use cli::{Action::*, CommandLineArgs};
+fn find_default_task_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".task_file.json");
+        path
+    })
+}
 
-fn main() -> Result<(), std::io::Error> {
+fn main() -> anyhow::Result<()> {
     let CommandLineArgs { action, task_file } = CommandLineArgs::from_args();
 
-    let task_file = task_file.expect("Failed to find task file !!");
+    let task_file = task_file
+        .or_else(find_default_task_file)
+        .ok_or(anyhow!("Failed to find task file !!"))?;
 
     match action {
         Add { text } => tasks::add_task(task_file, tasks::Task::new(text)),
         List => tasks::list_tasks(task_file),
         Done { position } => tasks::complete_task(task_file, position),
-    }
+    }?;
+
+    Ok(())
 }
